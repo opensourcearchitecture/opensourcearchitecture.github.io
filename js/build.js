7bece092593b5a0bf747f2c0f6734a34ca3c3755
chismi <chisholm.mitchell@gmail.com>
2020-03-03 00:16:12 -0500

    cleanup

--- js/build.js
+++ js/build.js
@@ -13,10 +13,3 @@ $description_holder.click(function () {
   $description_holder.css("display", "none");
 });
 "use strict";
-
-var $, fill;
-$ = require('jquery');
-(fill = function fill(item) {
-  return $('.tagline').append("".concat(item));
-})('The most creative minds in Art');
-fill;
